a43c4cdf07e41cf8c4006e2be5d527fc8b1ff130
ohtero <91533120+Tuhto@users.noreply.github.com>
2022-10-03 17:44:41 +0300

    Luotiin perusrakenne ja rivien lisäy/poisto

--- scripts.js
+++ scripts.js
@@ -1,15 +1,42 @@
 const inputField = document.getElementById("input-textfield"); 
 
-var appendItem = () => {
-    const itemList = document.getElementById("item-list");
-    const newItem = document.createElement("li");
-    newItem.innerHTML = inputField.value;
-    itemList.appendChild(newItem);
-    inputField.value = " ";
+// Tarkistaa "add"-nappia painettaessa, onko syöttörivin arvo > 0. Jos ei, ilmoittaa virheestä.
+
+function checkFieldValue() {
+    if (inputField.value.length > 0) {
+        appendItem();
+    } else (alert("Et voi lisätä tyhjää riviä!"))
 };
 
-const addBtn = document.getElementById("add-item-btn");
-addBtn.addEventListener("click", appendItem);
+document.getElementById("add-item-btn").addEventListener("click", checkFieldValue);
 
+// Lisää uuden <li>-elementin tekstisyötteen pohjalta. Luo ensin uuden <li> elementin, ja luo sen sisään 
+// <button>-elementin.
+
+function createButton() {                                   
+    const newBtn = document.createElement("button");
+    newBtn.className = "del-row-btn";
+    newBtn.innerHTML = "Del";
+    newBtn.addEventListener("click", deleteRow);
+    return newBtn;
+};
+
+function createListItem() {
+    const newItem = document.createElement("li");
+    newItem.className = "flex a-center";
+    newItem.innerHTML = inputField.value;
+    newItem.appendChild(createButton());
+    return newItem;
+};
+
+function appendItem() {                                        
+    const itemList = document.getElementById("item-list");
+    itemList.appendChild(createListItem());
+    inputField.value = "";
+};
+
+function deleteRow() {
+    this.parentElement.remove();
+};
 
 
