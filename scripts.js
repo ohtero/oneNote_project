d2e9be690a9f4fad53fd407eacd3300cb7324c89
ohtero <91533120+Tuhto@users.noreply.github.com>
2022-10-08 15:48:59 +0300

    Update scripts.js
    Attempted refactoring code

--- scripts.js
+++ scripts.js
@@ -1,149 +1,102 @@
-const inputField = document.getElementById("input-textfield"); 
 
-// Tarkistaa "add"-nappia painettaessa, onko syöttörivin arvo > 0. Jos ei, ilmoittaa virheestä.
+// New list row with included elements
 
-const modal = document.getElementById('alert-modal');       // <dialog>-elementti, joka avautuu sivun "päälle"
-const closeModal = document.getElementById('close-modal-btn');  // <dialog>-elementin sulkunappi    
+const newListRow = `<li>
+    <div class="checkbox-container">
+        <input type="checkbox" class="list-checkbox" name="checkbox">
+    </div>
+    <input type="text" class="list-textfield" disabled>
+    <div class="option-menu">
+        <button class="option-btn" ><i class="bi bi-three-dots"></i></button>
+        <div class="option-items">
+            <button class="edit-button" id="edit"><i class="bi bi-pencil"></i></button>
+            <button class="del-row-btn"><i class="bi bi-trash"></i></button>
+        </div>
+    </div>
+</li>`
 
-function checkFieldValue() {
+
+// Globar vars
+
+const inputField = document.getElementById('input-textfield');
+const modal = document.getElementById('alert-modal');
+
+// Checks if input length for adding list items is > 0. If it is, proceeds to add list row 
+
+const checkValue = () => {
     if (inputField.value.length > 0) {
-        appendItem();
-    } else modal.showModal();    // avaa dialogin      
+        addListRow();
+    } else modal.showModal();  
 };
 
-closeModal.addEventListener('click', () => modal.close());  //sulkee dialogin
+// Adds new list and functionalities for the buttons on the row
 
-document.getElementById("add-item-btn").addEventListener("click", checkFieldValue);
-
-
-//**************************************//
-//            Lisäysfukntiot            //
-//**************************************//
-
-
-// Luo tekstisyötteen pohjalta uuden <li>-elementin, ja uudet elementit sen sisään 
-
-
-
-
-
-
-
-function createDelImg() {
-    const newImg = document.createElement('i');
-    newImg.className = 'bi bi-trash';
-    return newImg;
-};
-
-function createDelButton() {                                   
-    const newBtn = document.createElement("button");
-    newBtn.className = "del-row-btn";
-  
-    newBtn.appendChild(createDelImg());
-    newBtn.addEventListener("click", deleteRow);
-    return newBtn;
-};
-
-function createEditImg() {
-    const newImg = document.createElement('i');
-    newImg.className = 'bi bi-pencil';
-    return newImg;
-};
-
-function createEditButton() {                                   
-    const newBtn = document.createElement("button");
-    newBtn.className = "edit";
-
-    newBtn.appendChild(createEditImg());
-    newBtn.addEventListener("click", editText);
-    return newBtn;
-};
-
-function createOptionItems() {
-    const newItems = document.createElement('div');
-    newItems.className = 'option-items';
-    newItems.appendChild(createEditButton());
-    newItems.appendChild(createDelButton());
-    return newItems;
-};
-
-function createEllipsesImg() {
-    const newImg = document.createElement('i');
-    newImg.className = 'bi bi-three-dots';
-    return newImg;
-};
-
-function createOptionBtn() {
-    const newBtn = document.createElement('button');
-    newBtn.className = 'option-btn';
-    newBtn.appendChild(createEllipsesImg());
-    return newBtn;
-};
-
-function createOptionMenu() {
-    const newMenu = document.createElement('div');
-    newMenu.className = 'option-menu';
-    newMenu.appendChild(createOptionBtn());   
-    newMenu.appendChild(createOptionItems()); 
-    return newMenu;
-};
-
-function createTextfield() {
-    const newField = document.createElement('input');
-    newField.type = 'text';
-    newField.className = 'list-textfield';
-    newField.value = inputField.value;
-    newField.setAttribute('disabled', '');
-    newField.addEventListener('blur', disableTextfield);
-    newField.addEventListener('keydown', dropFocus);
-    return newField;
-};
-
-
-
-function createCheckbox() {
-    const newCheckbox = document.createElement('input');
-    newCheckbox.type = 'checkbox';
-    newCheckbox.className = 'list-checkbox';
-    newCheckbox.name = 'checkbox';
-    newCheckbox.addEventListener('click', checkState);
-    return newCheckbox;
-};
-
-function createCheckboxContainer() {
-    const newContainer = document.createElement('div');
-    newContainer.className = 'checkbox-container';
-    newContainer.appendChild(createCheckbox());
-    return newContainer;
-};
-
-function createListItem() {
-    const newItem = document.createElement("li");
-    newItem.appendChild(createCheckboxContainer());
-    newItem.appendChild(createTextfield());
-    newItem.appendChild(createOptionMenu());
-  
-    return newItem;
-};
-
-function appendItem() {                                        
-    const itemList = document.getElementById("item-list");
-    itemList.appendChild(createListItem());
+function addListRow() {
+    const list = document.getElementById('item-list');
+    list.innerHTML += newListRow;
+    const textField = list.querySelector('li > input:first-of-type');
+    textField.value = inputField.value;
     inputField.value = "";
+    addFieldLocking(textField);
+    addEditFunction();
+    addDelFunction();
 };
 
-//----------- lisäysmetodit loppuu -----------//
 
 
-//**************************************//
-//            Event-funktiot            //
-//**************************************//
 
-// poistaa rivin 
+//ei toimi syysta X
 
-function deleteRow() {              
-    this.parentElement.parentElement.parentElement.remove();  //muuta kohdistus
+const addFieldLocking = (textField) => {        
+    textField = textField;      
+    for (var i = 0; i < textField.length; i++)
+    textField[i].addEventListener('onblur', console.log('jee'));
+}; 
+
+
+// Adds functionality to the 'Delete' button
+
+const addDelFunction = () => {
+    const delButtons = document.getElementsByClassName('del-row-btn');
+    for (var i = 0;i < delButtons.length; i++) {
+        delButtons[i].addEventListener("click", function deleteRow() {              
+            this.parentElement.parentElement.parentElement.remove();
+        });
+    };
 };
+
+// Adds functionality to the 'Edit' button
+
+const addEditFunction = () => {
+    const editButtons = document.getElementsByClassName('edit-button');
+    for (var i = 0;i < editButtons.length; i++) {
+        editButtons[i].addEventListener("click", function editText() {
+            const textfield = this.parentElement.parentElement.previousElementSibling;           
+            textfield.removeAttribute('disabled');
+            textfield.focus();
+        });
+    };
+};
+
+
+
+
+// Event listeners
+
+document.getElementById("add-item-btn").addEventListener("click", checkValue);
+document.getElementById('close-modal-btn').addEventListener('click', () => modal.close());
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Muuttaa listan tekstikentän väriä riippuen, onko ruutu merkattu 
@@ -179,6 +132,75 @@ function dropFocus (e) {
     };
 };
 
-//----------- Event-funktiot loppuu -----------//
 
 
+
+function createCheckbox() {
+    const newCheckbox = document.createElement('input');
+    newCheckbox.type = 'checkbox';
+    newCheckbox.className = 'list-checkbox';
+    newCheckbox.name = 'checkbox';
+    newCheckbox.addEventListener('click', checkState);
+    return newCheckbox;
+};
+
+function createCheckboxContainer() {
+    const newContainer = document.createElement('div');
+    newContainer.className = 'checkbox-container';
+    newContainer.appendChild(createCheckbox());
+    return newContainer;
+};
+
+function createListItem() {
+    const newItem = document.createElement("li");
+    newItem.appendChild(createCheckboxContainer());
+    newItem.appendChild(createTextfield());
+    newItem.appendChild(createOptionMenu());
+  
+    return newItem;
+};
+
+function appendItem() {                                        
+    const itemList = document.getElementById("item-list");
+    itemList.appendChild(createListItem());
+    inputField.value = "";
+};
+
+
+
+//----------- lisäysmetodit loppuu -----------//
+
+
+//**************************************//
+//            Event-funktiot            //
+//**************************************//
+
+// poistaa rivin 
+
+function deleteRow() {              
+    this.parentElement.parentElement.parentElement.remove();  //muuta kohdistus
+};
+
+
+// Muuttaa listan tekstikentän väriä riippuen, onko ruutu merkattu 
+
+function checkState() {
+    const textfield = this.parentElement.nextElementSibling;
+    if (this.checked) {
+        textfield.style['background-color'] = '#ccc';
+        textfield.style['color'] = '#777';
+    } else {textfield.style['background-color'] = '#fff';
+            textfield.style['color'] = '#111';};
+};
+
+// Avaa tekstikentän editoimista varten
+
+
+
+// Lukitsee tekstikentän
+
+function dropFocus (e) {
+    if (e.key == 'Enter') {
+    this.blur(); 
+    };
+}
